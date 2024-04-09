33d288d6ef2449fb0dba5e6c91e9da8545e00732
Wendler Custódio <wendlermoa@gmail.com>
2024-04-09 19:38:18 -0300

    Interfaces Implement

--- myfinance-web-dotnet-service/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace myfinance_web_dotnet_service;
-
-public class Class1
-{
-
-}
---
+++ myfinance-web-dotnet-service/TransacaoService.cs
@@ -0,0 +1,51 @@
+using Microsoft.EntityFrameworkCore;
+using myfinance_web_dotnet_domain.Entities;
+using myfinance_web_dotnet_infra;
+using myfinance_web_dotnet_service.Interfaces;
+
+namespace myfinance_web_dotnet_service
+{
+    public class TransacaoService : ITransacaoService
+    {
+        private readonly MyFinanceDbContext _dbContext;
+        public TransacaoService(MyFinanceDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void Cadastrar(Transacao Entidade)
+        {
+            var dbSet = _dbContext.Transacao;
+
+            if(Entidade.Id == null)
+            {
+                dbSet.Add(Entidade);
+            }
+            else
+            {
+                dbSet.Attach(Entidade);
+                _dbContext.Entry(Entidade).State = EntityState.Modified;
+            }
+
+            _dbContext.SaveChanges();
+        }
+
+        public void Excluir(int Id)
+        {
+            var Transacao = new Transacao(){Id = Id};
+            _dbContext.Attach(Transacao);
+            _dbContext.Remove(Transacao);
+            _dbContext.SaveChanges();
+        }
+
+        public List<Transacao> ListarRegistros()
+        {
+            var dbSet = _dbContext.Transacao;
+            return dbSet.ToList();
+        }
+
+        public Transacao RetornarRegistro(int Id)
+        {
+            return _dbContext.Transacao.Where(x => x.Id == Id).First();
+        }
+    }
+}
